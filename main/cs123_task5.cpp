677fcefa36a4b6b7e65b51aa5d145e1f1e045258
reachdevikas <79503043+reachdevikas@users.noreply.github.com>
2023-02-19 21:18:27 +0530

    Create cs123_task5.cpp

---
+++ main/cs123_task5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hiii!! \n";
+  cout << "This is Task 5\n";
+  cout << "I am PES1UG20CS123: Devika S\n";
+  cout << "Pipelines\n";
+  cout << "Byeee\n";
+  return 0;
+}
